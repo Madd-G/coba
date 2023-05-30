98555b092690e039458a3c54a9d447a9ca6367be
Madd-G <akhmadnuralamsyah@gmail.com>
2023-05-30 18:43:10 +0700

    feature/add fifth screen

---
+++ lib/fifth_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FifthScreen extends StatelessWidget {
+  const FifthScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Fifth Screen'),
+      ),
+    );
+  }
+}
